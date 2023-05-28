ff7e081e6aa57b45dc27f5b49e19aad335299cd1
Notwacob <wilson.a.jacob@gmail.com>
2023-05-28 02:57:34 -0400

    First Commit

---
+++ src/content/about.js
@@ -0,0 +1,12 @@
+export const about = [
+  {
+    title: "Web Developer",
+    duration: "Aug 2018 - Sept 2022",
+    icon: "/assets/about/experience.png",
+  },
+  {
+    title: "Virginia Wesleyan University",
+    duration: "Aug 2019 - May 2023",
+    icon: "/assets/about/graduation.png",
+  },
+];
