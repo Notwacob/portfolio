ad3aa1444b618f6e19f3a2b4bdce1164da76e607
Notwacob <wilson.a.jacob@gmail.com>
2023-06-02 13:21:52 -0400

    Update activities.js
    added link to my Javascript 30 projects

--- src/content/activities.js
+++ src/content/activities.js
@@ -3,7 +3,7 @@ export const activities = [
     title: "Javascript 30",
     text: "Build 30 things in 30 days with 30 tutorials",
     text2: "No Frameworks X No Compilers X No Libraries X No Boilerplate",
-    link: "",
+    link: "https://notwacob.github.io/javascript30/",
   },
   {
     title: "Coding Challenges",
